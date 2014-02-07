d8f848dff430db3a6619ce74575f648b98497d31
roszczer <roszczer@gmail.com>
2014-02-07 09:29:11 +0100

    Finish user edit, update, index and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }  
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
---
+++ features/edit_user.feature
@@ -0,0 +1,6 @@
+Feature: Edit User
+	Scenario: Edit page
+  	Given a user visits the edit user page
+  		And the user has an account
+  	When the user visits the edit user page
+  	Then they should see 
